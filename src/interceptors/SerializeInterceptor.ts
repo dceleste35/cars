d3585548822ebc1b20728659a5099b8765c0c51a
dceleste35 <danceleste974@gmail.com>
2024-06-11 12:55:36 +0200

    Creation du SerializeInterceptor

---
+++ src/interceptors/SerializeInterceptor.ts
@@ -0,0 +1,19 @@
+import {
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class SerializeInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        console.log('I am running before the handler', context);
+
+        return next.handle().pipe(
+            map((data: any) => {
+                console.log("I'm running before the response is sent out", data);
+            }),
+        );
+    }
+}
