9fdea18a2c09299c29a0bcdbdd8de53260e15e00
dceleste35 <danceleste974@gmail.com>
2024-06-12 11:40:51 +0200

    Affiche l'enssemble des routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import cookieSession = require('cookie-session');
 
 async function bootstrap() {
@@ -8,6 +8,23 @@ async function bootstrap() {
   app.use(cookieSession({ keys: ['dodo'] }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
+
+  await displayRoutes(app);
+}
+
+async function displayRoutes(app: INestApplication) {
+    const server = app.getHttpServer();
+    const router = server._events.request._router;
+    const availableRoutes: [] = router.stack
+        .map(layer => {
+            if (layer.route) {
+                return {
+                route: { path: layer.route?.path, stack: layer.route.stack[0].method }
+                };
+            }
+        })
+        .filter(item => item !== undefined);
+    console.log(availableRoutes);
 }
 
 bootstrap();
